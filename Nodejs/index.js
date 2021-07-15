5c2ccf5dec85896ff84d17e6c243ef102a76732b
上島春輝 <h.ueji.43@gmail.com>
2021-07-15 21:35:55 +0900

    Node.js 追記

--- Nodejs/index.js
+++ Nodejs/index.js
@@ -1,2 +1,9 @@
-// テスト Hello World !　
-console.log("Hello World !");
+// 未定義の変数の使用を禁止するなど、より厳格な文法でプログラムを書くことができる。
+'use strict';
+
+// main関数
+main()
+
+function main() {
+    console.log('Hello World!')
+}
